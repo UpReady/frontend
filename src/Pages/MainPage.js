5638f834c365e3b8099241fbfca1835123795863
Aysel Ünlü <aysell.unlu48@gmail.com>
2023-07-22 18:54:03 +0300

    Create MainPage-> Highlights

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HighLights from "../Components/HighLights";
 import MainArea from "../Components/MainArea";
 
 function MainPage() {
   return (
     <React.Fragment>
       <MainArea/>
+      <HighLights/>
     </React.Fragment>
   );
 }
